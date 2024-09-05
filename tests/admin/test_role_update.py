5a67a15d2019e93e5ffd8097dd9937338e7fb0f0
DX3906 <3065231714@qq.com>
2024-09-05 19:19:33 +0800

    实现对admin_role_update接口的参数测试

--- tests/admin/test_role_update.py
+++ tests/admin/test_role_update.py
@@ -5,9 +5,10 @@ from requests import request
 from conftest import get_token
 from utils.yaml_handler import YamlHandler
 
-yamlhander_data = YamlHandler("E:/code/python/mall_tiny_test/data/admin/role_update.yaml")
+yamlhander_data = YamlHandler("E:/code/python/mall_tiny_test/data/admin/role_update_data.yaml")
 role_update_data = yamlhander_data.read_yaml()
 
+@allure.link("/admin/role/update")
 @allure.feature("admin:给用户分配角色")
 @pytest.mark.parametrize("case", role_update_data)
 def test_role_update_data(case, get_evn, get_root_token, get_token):
@@ -22,5 +23,32 @@ def test_role_update_data(case, get_evn, get_root_token, get_token):
         headers["Authorization"] = get_root_token
 
     r = request(method, url=host + path, headers=headers)
-    assert r.json()["message"] == case["预期结果"]
+    if "message" in r.json():
+        assert r.json()["message"] == case["预期结果"]
+    else:
+        assert r.json()["error"] == case["预期结果"]
+    allure.dynamic.title(case["用例编号"])
+
+yamlhander_param=YamlHandler("E:/code/python/mall_tiny_test/data/admin/role_update_param.yaml")
+role_update_param=yamlhander_param.read_yaml()
+
+@allure.link("/admin/role/update")
+@allure.feature("admin:给用户分配角色")
+@pytest.mark.parametrize("case",role_update_param)
+def test_role_update_param(case,get_evn,get_token,get_root_token):
+
+    host = get_evn["host"]
+    path = case["URL"]
+    method = case["请求方法"]
+    headers = get_evn["headers"]
+    if case["用例标题"] == "没有相关权限":
+        headers["Authorization"] = get_token
+    else:
+        headers["Authorization"] = get_root_token
+
+    r = request(method, url=host + path, headers=headers)
+    if "message" in r.json():
+        assert r.json()["message"] == case["预期结果"]
+    else:
+        assert r.json()["error"] == case["预期结果"]
     allure.dynamic.title(case["用例编号"])
