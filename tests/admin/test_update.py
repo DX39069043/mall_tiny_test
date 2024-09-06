9c51014029f5f42dbafe5c0939862e7774dd8098
DX3906 <3065231714@qq.com>
2024-09-06 11:16:18 +0800

    实现了对admin_update接口的测试

---
+++ tests/admin/test_update.py
@@ -0,0 +1,54 @@
+import allure
+import pytest
+import requests
+
+from conftest import get_root_token
+from utils import yaml_handler
+
+yamlhandler_data = yaml_handler.YamlHandler("E:/code/python/mall_tiny_test/data/admin/update_data.yaml")
+update_data = yamlhandler_data.read_yaml()
+
+@allure.feature("admin:修改指定用户信息")
+@allure.link("/admin/update")
+@pytest.mark.parametrize("case", update_data)
+def test_update_date(case,get_evn,get_token,get_root_token):
+
+    host = get_evn["host"]
+    headers = {**get_evn["headers"],**case["请求头"]}
+    if case["用例标题"]=="没有相关权限":
+        headers["Authorization"] = get_token
+    else:
+        headers["Authorization"] = get_root_token
+
+    method=case["请求方法"]
+    path = case["URL"]
+
+    r = requests.request(method, url=host + path, headers=headers, json=case["请求体"])
+    if "message" in r.json():
+        assert r.json()["message"] == case["预期结果"]
+    else:
+        assert r.json()["error"] == case["预期结果"]
+    allure.dynamic.title(case["用例编号"])
+
+
+yamlhandler_param = yaml_handler.YamlHandler("E:/code/python/mall_tiny_test/data/admin/update_param.yaml")
+update_param = yamlhandler_param.read_yaml()
+
+
+@allure.feature("admin:修改指定用户信息")
+@allure.link("/admin/update")
+@pytest.mark.parametrize("case", update_param)
+def test_update_param(case,get_evn,get_root_token):
+
+    host = get_evn["host"]
+    headers = {**get_evn["headers"],**case["请求头"]}
+    headers["Authorization"] = get_root_token
+    method=case["请求方法"]
+    path = case["URL"]
+
+    r = requests.request(method, url=host + path, headers=headers, json=case["请求体"])
+    if "message" in r.json():
+        assert r.json()["message"] == case["预期结果"]
+    else:
+        assert r.json()["error"] == case["预期结果"]
+    allure.dynamic.title(case["用例编号"])
